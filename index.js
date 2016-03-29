d0a947a78a4d91e1d355150c828903284203ab09
ba55ie <ba55ie@users.noreply.github.com>
2016-03-29 10:30:37 +0200

    Variables

--- index.js
+++ index.js
@@ -113,14 +113,14 @@ module.exports = {
 		'no-catch-shadow': 0,
 		'no-delete-var': 2,
 		'no-label-var': 2,
-		'no-restricted-globals': [2, 'event'],
+		'no-restricted-globals': 0,
 		'no-shadow': 2,
 		'no-shadow-restricted-names': 2,
-		'no-undef': [2, { typeof: true }],
+		'no-undef': 2,
 		'no-undef-init': 2,
 		'no-undefined': 2,
 		'no-unused-vars': 2,
-		'no-use-before-define': [2, 'nofunc'],
+		'no-use-before-define': 2,
 
 		// Node.js
 		'callback-return': [2, ['cb', 'callback', 'next', 'done']],
