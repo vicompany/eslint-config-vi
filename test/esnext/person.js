49e10fc80690c19c480f8a2d4c54f7e3f95465f5
ba55ie <ba55ie@users.noreply.github.com>
2017-10-17 10:00:22 +0200

    Update to 4.9.0 and add rules

--- test/esnext/person.js
+++ test/esnext/person.js
@@ -1,9 +1,21 @@
+// This config is used to decide if a person is allowed to drink.
+// We use 'separate-lines' for multiline comments
+// Read more at https://eslint.org/docs/rules/multiline-comment-style
 const config = {
 	'drinks': ['beer', 'gin', 'tequila'],
 	'drinking-age': 21,
 };
 
+// The following comments are in JSDoc format
+// which are always allowed. See http://usejsdoc.org/
+
+/** Class representing a person. */
 class Person {
+	/**
+	 * Create a person.
+	 * @param {string} name - The name of the person.
+	 * @param {int} age - The age of the person.
+	 */
 	constructor(name, age) {
 		this.name = name;
 		this.age = age;
